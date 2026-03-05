39d451fed6214a9fa3c452487ec341026079af28
Svitlana Panchak <svitlanapanchak21@gmail.com>
2026-03-05 21:41:12 +0200

    Додано unit та e2e тести, перевірено покриття

---
+++ tests/todo.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('Додавання завдання через форму', async ({ page }) => {
+  await page.goto('http://localhost:3000'); 
+
+  const input = page.getByPlaceholder(/Add item..../i);
+  await input.fill('Написати звіт по лабі');
+
+  await page.getByText('+').click();
+
+  await expect(page.locator('text=Написати звіт по лабі')).toBeVisible();
+});
